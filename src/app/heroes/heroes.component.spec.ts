852b2ea062802b5da5f21cc779afd671bb5535ce
Shane Reeve <shanereeve@gmail.com>
2022-08-07 14:17:48 -0600

    Testing a Component and mocking isolated Code

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,32 @@
+import { of } from "rxjs";
+
+import { HeroesComponent } from "./heroes.component";
+
+describe('HeroesComponet', () => {
+  let component: HeroesComponent;
+  let HEROES;
+  let mockHeroSerivice;
+
+  beforeEach(()=> {
+    HEROES = [
+      {id:1, name: 'SpiderDude', strength: 8},
+      {id:2, name: 'Wonderful Woman', strength: 24},
+      {id:3, name: 'SuperDude', strength: 55}
+    ]
+
+    mockHeroSerivice = jasmine.createSpyObj(['getHeros', 'addHero', 'deleteHero'])
+
+    component = new HeroesComponent(mockHeroSerivice)
+  })
+
+  describe('delete', () => {
+    it('should remove the indicated hero from the heroes list', () => {
+      mockHeroSerivice.deleteHero.and.returnValue(of(true))
+      component.heroes = HEROES;
+
+      component.delete(HEROES[2]);
+
+      expect(component.heroes.length).toBe(2)
+    } )
+  })
+})
