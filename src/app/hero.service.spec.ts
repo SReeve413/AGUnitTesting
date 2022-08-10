2fd72dcf4b87bfe0e92b8c435371c0989067c2a7
Shane Reeve <shanereeve@gmail.com>
2022-08-09 20:40:33 -0600

    Integration Testing os Services and using Inject helper fuction

---
+++ src/app/hero.service.spec.ts
@@ -0,0 +1,36 @@
+import { inject, TestBed } from "@angular/core/testing"
+import { HeroService } from "./hero.service"
+import { MessageService } from "./message.service"
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing"
+
+describe('HeroService', () => {
+let mockMessageService;
+let httpTestingController: HttpTestingController;
+let service: HeroService;
+
+  beforeEach(() => {
+    mockMessageService = jasmine.createSpyObj(['add'])
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        HeroService,
+        {provide: MessageService, useValue: mockMessageService}
+      ]
+    })
+
+    httpTestingController = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(HeroService);
+
+  })
+
+  describe('getHero', ()=> {
+    it('should call get with the correct URL', inject(
+      [HeroService],
+      (service: HeroService, controller: HttpTestingController) => {
+      //call getHero()
+      service.getHero(4);
+      //test that the URL was correct
+
+    }))
+  })
+})
