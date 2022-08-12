0b2667a265a937dd16f7da4f32cf26182481a070
Shane Reeve <shanereeve@gmail.com>
2022-08-11 19:35:45 -0600

    Testing the RouterLinK

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -79,7 +79,6 @@ describe('HeroesComponent (Deep Tests)', ()=>{
     heroComponents[0].triggerEventHandler('delete', null);
 
     expect(fixture.componentInstance.delete).toHaveBeenCalledWith(HEROES[0]);
-
   })
 
   it('should add a new hero to the hero list when the add button is clicked', () => {
@@ -97,6 +96,18 @@ describe('HeroesComponent (Deep Tests)', ()=>{
 
     const heroText = fixture.debugElement.query(By.css('ul')).nativeElement.textContent;
     expect(heroText).toContain(name);
+  })
 
+  it('should have the correct route for the first hero', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+    const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
+    let routerLink = heroComponents[0]
+      .query(By.directive(RouterLinkDirectiveStub))
+      .injector.get(RouterLinkDirectiveStub);
+
+    heroComponents[0].query(By.css('a')).triggerEventHandler('click', null);
+
+    expect(routerLink.navigateTo).toBe('/detail/1')
   })
 })
